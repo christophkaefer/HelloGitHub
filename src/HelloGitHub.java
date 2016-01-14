d44963dd8b9f96a0b3dd2cf6721091b627e44e19
christoph.kaefer <christoph.kaefer@edu.campus02.at>
2016-01-14 22:00:57 +0100

    kleine Änderung vom zweiten Entwickler

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -5,7 +5,7 @@ public class HelloGitHub
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-		System.out.println("Hallo GitHub!");
+		System.out.println("Hallo GitHub, ich bin der zweite Entwickler!");
 
 	}
 
